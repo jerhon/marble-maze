4d7b373b25b0c1fb7b22c8b8934157f53698988b
Jeremy Honl <jerhon@users.noreply.github.com>
2019-09-05 23:31:40 -0500

    Add collision and motion

--- src/game.ts
+++ src/game.ts
@@ -24,8 +24,8 @@ interface Point {
 }
 
 export class GameScene extends Phaser.Scene {
-  private player: Phaser.GameObjects.Arc ;
-  private end: Phaser.GameObjects.Arc;
+  private marble1: Phaser.GameObjects.Arc & { body : Phaser.Physics.Arcade.Body } ;
+  private marble2: Phaser.GameObjects.Arc  & { body : Phaser.Physics.Arcade.Body };
   private walls: Phaser.GameObjects.Group;
   private height: number;
   private width: number;
@@ -35,12 +35,20 @@ export class GameScene extends Phaser.Scene {
   private wallYOffset: number;
   private radius: number;
   private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+  private beta: number = 0;
+  private gamma: number = 0;
+  private collider: Phaser.Physics.Arcade.Collider;
+  private collider2: Phaser.Physics.Arcade.Collider;
 
   constructor() {
     super(sceneConfig);
   }
   
   public create(data: maze) {
+    if ((window as any).DeviceOrientationEvent) {
+      window.addEventListener("deviceorientation", this.deviceOrientationChanged.bind(this));
+    }
+
     this.cursorKeys = this.input.keyboard.createCursorKeys()
 
     this.height = this.game.canvas.height;
@@ -65,10 +73,17 @@ export class GameScene extends Phaser.Scene {
     this.radius = Math.min( this.wallWidth / 3, this.wallHeight / 3);
     
     let startCoords = this.getCoordinates({x: data.startingPosition[0], y: data.startingPosition[1]});
-    this.player = this.add.circle(startCoords.x, startCoords.y, this.radius, 0xAAAAAA);
+    this.marble1 = this.add.circle(startCoords.x, startCoords.y, this.radius, 0xAAAAAA) as any;
+    this.physics.add.existing(this.marble1);
+    this.marble1.body.setCollideWorldBounds(true);
 
     let endCoords = this.getCoordinates({x: data.endingPosition[0], y: data.endingPosition[1]});
-    this.end = this.add.circle(endCoords.x, endCoords.y, this.radius, 0xFF0000);
+    this.marble2 = this.add.circle(endCoords.x, endCoords.y, this.radius, 0xFF0000) as any;
+    this.physics.add.existing(this.marble2);
+    this.marble2.body.setCollideWorldBounds(true);
+
+    this.physics.add.collider([this.marble1, this.marble2], this.walls);
+    this.physics.add.overlap(this.marble1, this.marble2);
   }
 
   getCoordinates(p: Point): Point {
@@ -80,6 +95,27 @@ export class GameScene extends Phaser.Scene {
   
   public update(time: number, delta: number) {
     // TODO: add motion of player, and end goal of combining the marbles
-    
+    for (let marble of [this.marble1, this.marble2]) {
+      marble.body.setVelocityX( (this.gamma / 180) * 300 );
+      marble.body.setVelocityY( (this.beta / 180) * 300 );
+
+      if (this.cursorKeys.down.isDown) {
+        marble.body.setVelocityY(300);
+      } else if (this.cursorKeys.up.isDown) {
+        marble.body.setVelocityY(-300);
+      }
+      
+      if (this.cursorKeys.left.isDown) {
+        marble.body.setVelocityX(-300);
+      } else if (this.cursorKeys.right.isDown) {
+        marble.body.setVelocityX(300);
+      }
+    }
+
+  }
+
+  public deviceOrientationChanged(evt: DeviceOrientationEvent) {
+    this.beta = evt.beta;
+    this.gamma = evt.gamma;
   }
 }
