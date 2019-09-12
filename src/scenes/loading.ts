a096e24a13a61f70dbf4e538bd6fff5dcc9bc1c8
Jeremy Honl <jerhon@users.noreply.github.com>
2019-09-11 22:38:01 -0500

    fix level not loading

--- src/scenes/loading.ts
+++ src/scenes/loading.ts
@@ -13,20 +13,21 @@ export class LoadingScene extends Phaser.Scene {
     constructor() {
       super(loadingConfig);  
     }
+    sizes = [10,20,30,40,60,100,120,150,200];
   
     public create(data: LoadingData) {
-      let level = 1;
+      let level = 0;
       if (data.level) {
         level = data.level;
       }
-      let size = 9 + level;
+      let size = this.sizes[level % this.sizes.length];
       console.log('level', level, 'data', data);
 
       // Use the GitHub API to fetch the Maze Data
       fetch('https://api.noopschallenge.com/mazebot/random?minSize=' + size +  '&maxSize=' + size)
         .then((r) =>
           r.json().then((r1) => {
-            console.log(r1);
+            console.log('level', level);
             this.scene.start('Game', {maze: r1, level});
           })
         );
