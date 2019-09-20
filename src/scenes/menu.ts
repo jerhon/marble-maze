db29427f9d62ac56cd1c639453ff7d95ca288930
Jeremy Honl <jerhon@users.noreply.github.com>
2019-09-19 23:07:32 -0500

    Update menu screen, add instructions, and title

--- src/scenes/menu.ts
+++ src/scenes/menu.ts
@@ -2,7 +2,7 @@ import { GameStateMachine } from "./gameStateMachine";
 
 export const MENU_SCENE = 'Menu';
 
-const menuConfig : Phaser.Types.Scenes.SettingsConfig = {
+const menuConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
     visible: false,
     key: MENU_SCENE
@@ -14,77 +14,72 @@ export class MenuScene extends Phaser.Scene {
     currentOption = 0;
 
     constructor() {
-      super(menuConfig);  
+        super(menuConfig);
 
-      this.stateMachine = new GameStateMachine(this);
+        this.stateMachine = new GameStateMachine(this);
     }
 
     options = [
         'New Game',
-        //'Timed Game',
         'About'
     ]
     menuOptions: Phaser.GameObjects.Text[] = [];
-  
+
     public create() {
-        let selected = 0;
+        let top = 0;
+        this.add.text(25, 25, 'Marble Maze', { fontSize: '50px' });
+        top += 85;
+        top += 50;
+
         let idx = 0;
-        for (let opt of this.options) {
-            console.log(opt);
-            let optText = this.add.text(10, 10 + (idx * 70), opt, {
+        for (let opt of this.options) {           
+            let optText = this.add.text(10, top, opt, {
                 fontSize: '40px',
                 fontFamily: 'Arial',
                 color: '#ffffff',
-                align: 'center'
+                align: 'center',
             });
+            optText.setWordWrapWidth(this.game.canvas.width - 20, true);
+            optText.setFixedSize(this.game.canvas.width - 20, 0);
+
             optText.setInteractive();
             var callback = ((v1) => (pointer, gameObject) => this.optionPicked(v1))(opt)
             optText.on('pointerup', callback);
             this.menuOptions.push(optText);
-            idx++;
-        }
+            top += 80;
 
-        this.input.on('pointerdown', (pointer : Phaser.Input.Pointer) => {
-            console.log("it's down.");
-            var option = this.getMenuOptionFromPoint(pointer.worldX, pointer.worldY);
-            if (option) {
-                console.log(this.options[option]);
-            }
-        });
-        
-    }
-
-    getMenuOptionFromPoint(x:number, y:number) : number {
-        let currentOpt = Math.floor((y - 10) / 70);
-        if (currentOpt < 0 || currentOpt >= this.menuOptions.length) {
-            currentOpt = 0;
+            optText.on('pointerover', (pointer: Phaser.Input.Pointer) => {
+                this.currentOption = this.options.indexOf(opt);
+            });
+            optText.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+                this.optionPicked(opt);
+            })
         }
-        return currentOpt;
+        top += 50;
+
+        var directions = this.add.text(25, top, 'Lead the marble through the maze.  Hold the phone flat in relation to the ground and tilt your phone to move the marble through the phase.');
+        directions.setWordWrapWidth(this.game.canvas.width - 50, true);
+
     }
 
     public optionPicked(option: string) {
-        console.log('option', option);
         switch (option) {
             case "New Game":
-                this.stateMachine.startLoadingLevel({ })
+                this.stateMachine.startLoadingLevel({})
                 break;
             case "Timed Game":
-                this.stateMachine.startLoadingLevel({ })
+                this.stateMachine.startLoadingLevel({})
                 break;
         }
     }
 
     public update(time: number, delta: number) {
-
-        this.currentOption = this.getMenuOptionFromPoint(this.input.mousePointer.worldX, this.input.mousePointer.worldY);
-        
         for (let i = 0; i < this.menuOptions.length; i++) {
             if (i != this.currentOption) {
                 this.menuOptions[i].alpha = 1;
             }
         }
         this.menuOptions[this.currentOption].alpha = 1 - (time % 750) / 750;
-        
     }
-  
+
 }
